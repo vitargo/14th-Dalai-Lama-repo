e972ab049303595c6d92739696f71a22f500f596
Vitargo <redkov@supportyourapp.com>
2023-05-31 07:06:59 +0300

    update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -35,7 +35,7 @@ resource "aws_security_group" "lab2-p" {
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = "-1"
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
